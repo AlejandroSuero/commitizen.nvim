dc4548a0aa244d51f92d21b59eef6b6a6a85e9a7
AlejandroSuero <asuerome@gmail.com>
2024-06-17 21:35:18 +0200

    feat(types): added api and logger types

--- lua/commitizen/types.lua
+++ lua/commitizen/types.lua
@@ -1,2 +1,9 @@
 ---@class CommitizenOptions
----@field confirm_before_commit? boolean
+---@field confirm_before_commit? boolean: whether to ask for confirmation before committing.
+
+---@class CommitizenAPI
+---@field create_commit fun() creates a conventional commit.
+
+---@alias CommitType "feat"|"fix"|"docs"|"style"|"refactor"|"perf"|"test"|"chore"
+
+---@alias LogLevel "trace"|"debug"|"info"|"warn"|"error"|"off"
