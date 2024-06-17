21c5bc8e2751ccdd05ce0fd05f1726558720cc67
AlejandroSuero <asuerome@gmail.com>
2024-06-17 21:36:04 +0200

    feat: create api

---
+++ lua/commitizen/api.lua
@@ -0,0 +1,15 @@
+local log = require("commitizen.logger")
+local u = require("commitizen.utils")
+
+---@class CommitizenAPI
+local M = {}
+
+M.create_commit = function()
+  local commit_type = u.choose_commit_type()
+  if commit_type == nil then
+    log:warn("No commit type selected")
+    return
+  end
+end
+
+return M
