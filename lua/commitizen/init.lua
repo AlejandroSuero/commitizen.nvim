e91d3012857c3a2561e3853a75826431f2b1f4dd
AlejandroSuero <asuerome@gmail.com>
2024-06-17 21:36:54 +0200

    feat: added `api` and `commands` fn

--- lua/commitizen/init.lua
+++ lua/commitizen/init.lua
@@ -1,7 +1,15 @@
+local api = require("commitizen.api")
+local commands = require("commitizen.commands")
 local config = require("commitizen.config")
 
 ---@class Commitizen
 local M = {}
+
+---@class Commitizen
+---@field create_commit fun()
+M.create_commit = function()
+  api.create_commit()
+end
 
 ---@class Commitizen
 ---@field setup fun(opts: CommitizenOptions|nil)
@@ -9,6 +17,7 @@ local M = {}
 ---@param opts? CommitizenOptions
 M.setup = function(opts)
   config.setup(opts or {})
+  commands.setup()
 end
 
 return M
